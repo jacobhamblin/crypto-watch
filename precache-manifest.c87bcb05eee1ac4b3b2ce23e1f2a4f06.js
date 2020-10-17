61b75242d119a63fb278bd535887860827c1a690
Jacob Hamblin <jacob@hamblin.cc>
2020-10-17 13:58:51 -0700

    Updates

---
+++ precache-manifest.c87bcb05eee1ac4b3b2ce23e1f2a4f06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c476bb61d69705072806494a1684f6",
+    "url": "/crypto-watch/index.html"
+  },
+  {
+    "revision": "be33de379a1a0067b22c",
+    "url": "/crypto-watch/static/css/main.00d198cb.chunk.css"
+  },
+  {
+    "revision": "eb07cdd128d8b0d09519",
+    "url": "/crypto-watch/static/js/2.61b5c71d.chunk.js"
+  },
+  {
+    "revision": "c539cd54423426898ababe6867a114a8",
+    "url": "/crypto-watch/static/js/2.61b5c71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be33de379a1a0067b22c",
+    "url": "/crypto-watch/static/js/main.fbec8298.chunk.js"
+  },
+  {
+    "revision": "826cdc577f539df2ab0528860b5cb97e",
+    "url": "/crypto-watch/static/js/main.fbec8298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c6b74df779c9d435f5",
+    "url": "/crypto-watch/static/js/runtime-main.97ac54b8.js"
+  },
+  {
+    "revision": "cfed9fdd2e503c80de430ed0af89d78d",
+    "url": "/crypto-watch/static/media/About.cfed9fdd.md"
+  }
+]);
---
+++ static/media/About.cfed9fdd.md
@@ -0,0 +1,7 @@
+# About
+
+This project serves to recreate a success of years passed, [BTC-charts](https://github.com/jacobhamblin/BTC-charts). A few challenges with the prior project lead me to seek to remake the project from the ground up, including:
+
+- The API changed drastically and no longer has a free option.
+- Each of the libraries are very outdated.
+- A better dev toolset is now available in [create-react-app](https://reactjs.org/docs/create-a-new-react-app.html).
