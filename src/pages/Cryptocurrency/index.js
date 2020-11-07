ddb23bd9fd8f3789e265cc982a6b01c70bc12319
Jacob Hamblin <jacob@hamblin.cc>
2020-11-07 15:35:34 -0700

    simple request doesnt have a custom header

--- src/pages/Cryptocurrency/index.js
+++ src/pages/Cryptocurrency/index.js
@@ -8,14 +8,10 @@ const Cryptocurrency = ({}) => {
     return response.json();
   }
   useEffect(() => {
-    const url =
-      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=USD";
-    const fetchParams = {
-      headers: {
-        "X-CMC_PRO_API_KEY": process.env.REACT_APP_COINMARKETCAP_KEY
-      }
-    };
-    fetch(url, fetchParams)
+    const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=USD&CMC_PRO_API_KEY=${
+      process.env.REACT_APP_COINMARKETCAP_KEY
+    }`;
+    fetch(url)
       .then(res => res.json())
       .then(res => setAPIData(res));
   }, []);
