474ad113d2e90067849008a0d034b523e4eeaf5d
Jacob Hamblin <jacob@hamblin.cc>
2020-11-15 16:17:03 -0700

    big wip

--- src/pages/Cryptocurrency/index.js
+++ src/pages/Cryptocurrency/index.js
@@ -3,33 +3,48 @@ import React, { useEffect, useRef, useState } from "react";
 const Cryptocurrency = ({}) => {
   const [top10Coins, setTop10Coins] = useState({});
   const [APIData, setAPIData] = useState({});
-  const coinValues = useRef([]);
 
   async function getData(url, params) {
     const response = await fetch(url, params);
     return response.json();
   }
-  useEffect(() => {
+
+  function fetchCoins(exchanges) {
+    console.log('fetching coins')
     const top10url = "https://api.coinpaprika.com/v1/coins";
     fetch(top10url)
       .then(res => res.json())
-      .then(res => {
-        const top10Coins = res.slice(0, 10);
-        setTop10Coins(top10Coins);
-        console.log(top10Coins)
-        top10Coins.forEach(coinDict => {
-          const url = `https://api.coinpaprika.com/v1/coins/${coinDict.id}/markets`;
-          fetch(url)
-            .then(res => res.json())
-            .then(res => {
-              const markets = res.filter(market => market.pair.endsWith("USD")).slice(0, 10);
-              coinValues.current = coinValues.current.concat(markets)
-            });
-      });
-    })
-  }, []);
+      .then(res => fetchRates(exchanges, res.slice(0, 10)))
+  }
 
-  console.log(coinValues.current);
+  function fetchExchanges() {
+    console.log('fetching exchanges')
+    const exchangesURL = "https://api.coinpaprika.com/v1/exchanges";
+    fetch(exchangesURL)
+      .then(res => res.json())
+      .then(res => res.sort((a, b) => a.adjusted_rank - b.adjusted_rank).slice(0, 10))
+      .then(objArr => new Set(objArr.map(obj => obj.id)))
+      .then(top10Exchanges => fetchCoins(top10Exchanges))
+  }
+
+  function fetchRates(exchanges, coins) {
+    console.log('fetching rates')
+    let coinValues = [];
+    coins.forEach(coin => {
+      const url = `https://api.coinpaprika.com/v1/coins/${coin.id}/markets`;
+      fetch(url)
+        .then(res => res.json())
+        .then(markets => markets.filter(market => market.pair.endsWith("USD")))
+        .then(markets => markets.filter(market => exchanges.has(market.id)))
+        .then(markets => coinValues = coinValues.concat(markets))
+    })
+    setTop10Coins(coinValues);
+  }
+
+  useEffect(() => fetchExchanges(), []);
+
+  console.log('top10Coins');
+  console.log(top10Coins);
   return <div />;
 };
 
