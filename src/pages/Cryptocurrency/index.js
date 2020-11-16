f91b04b839828c63c19ab3a8b6896d70606b0fcf
Jacob Hamblin <jacob@hamblin.cc>
2020-11-16 02:36:12 -0700

    still wip

--- src/pages/Cryptocurrency/index.js
+++ src/pages/Cryptocurrency/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 
 const Cryptocurrency = ({}) => {
-  const [top10Coins, setTop10Coins] = useState({});
+  const [top10Coins, setTop10Coins] = useState([]);
   const [APIData, setAPIData] = useState({});
 
   async function getData(url, params) {
@@ -15,6 +15,7 @@ const Cryptocurrency = ({}) => {
     fetch(top10url)
       .then(res => res.json())
       .then(res => fetchRates(exchanges, res.slice(0, 10)))
+      .then(coins => setTop10Coins(coins))
   }
 
   function fetchExchanges() {
@@ -22,30 +23,38 @@ const Cryptocurrency = ({}) => {
     const exchangesURL = "https://api.coinpaprika.com/v1/exchanges";
     fetch(exchangesURL)
       .then(res => res.json())
-      .then(res => res.sort((a, b) => a.adjusted_rank - b.adjusted_rank).slice(0, 10))
+      .then(res => res.filter(exchange => !!exchange.adjusted_rank)
+      .sort((a, b) => a.adjusted_rank - b.adjusted_rank).slice(0, 10))
       .then(objArr => new Set(objArr.map(obj => obj.id)))
       .then(top10Exchanges => fetchCoins(top10Exchanges))
   }
 
-  function fetchRates(exchanges, coins) {
+  async function fetchRates(exchanges, coins) {
     console.log('fetching rates')
+    console.log(exchanges, coins)
     let coinValues = [];
-    coins.forEach(coin => {
+    coins.forEach(async coin => {
       const url = `https://api.coinpaprika.com/v1/coins/${coin.id}/markets`;
-      fetch(url)
+      await fetch(url)
         .then(res => res.json())
         .then(markets => markets.filter(market => market.pair.endsWith("USD")))
-        .then(markets => markets.filter(market => exchanges.has(market.id)))
-        .then(markets => coinValues = coinValues.concat(markets))
+        .then(markets => {console.log(markets); return markets;})
+        .then(markets => markets.filter(market => exchanges.has(market.exchange_id)))
+        .then(markets => {console.log(markets); return markets;})
+        .then(markets => {coinValues = coinValues.concat(markets);})
     })
-    setTop10Coins(coinValues);
+    console.log('about to set coins')
+    console.log(coinValues)
+    return coinValues;
   }
 
   useEffect(() => fetchExchanges(), []);
 
   console.log('top10Coins');
   console.log(top10Coins);
-  return <div />;
+  return <div>
+      {top10Coins.length}
+    </div>
 };
 
 export default Cryptocurrency;
